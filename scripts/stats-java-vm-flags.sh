0883b3c272a631d0e61b3de8a4ff240ff7c59bd0
chenzhihui <chenzhihui244@msn.com>
2017-11-22 16:22:45 +0800

    update

--- scripts/stats-java-vm-flags.sh
+++ scripts/stats-java-vm-flags.sh
@@ -10,6 +10,17 @@ function jvm_flags_stats {
 	local slist=`ps -o pid,psr,comm -xH | grep java | awk '{++stats[$1]} END{for (i in stats) {print stats[i]}}'`
 
 	echo "stats jvm flags" > $log_file
+	jps -m -l -v -V | sed 's/ /,/g' >> $log_file
+}
+
+function jvm_flags_stats_2 {
+	local output_dir=/local
+	local log_file=$output_dir/${1-"jvm_flags.txt"}
+
+	local plist=`ps -o pid,psr,comm -xH | grep java | awk '{++stats[$1]} END{for (i in stats) {print i}}'`
+	local slist=`ps -o pid,psr,comm -xH | grep java | awk '{++stats[$1]} END{for (i in stats) {print stats[i]}}'`
+
+	echo "stats jvm flags" > $log_file
 	echo "processes:" >> $log_file
 	echo $plist >> $log_file
 	echo "counts:" >> $log_file
